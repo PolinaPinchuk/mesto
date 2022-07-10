8d538134eff2ff02adb271879bb6df91c2402667
Polina Pinchuk <polinapinchuck@gmail.com>
2022-07-10 17:40:43 +0200

    mesto

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -1,14 +1,15 @@
 import { Popup } from "./Popup.js";
 
 export class PopupWithImage extends Popup {
-    open(text, link) {
+    constructor(popupSelector) {
+        super(popupSelector);
         this._image = this._popup.querySelector(".popup__image");
         this._caption = this._popup.querySelector(".popup__caption");
-
+        }
+    open(text, link) {
         this._image.src = link;
         this._image.alt = text;
         this._caption.textContent = text;
-
         super.open();
     }
 }
